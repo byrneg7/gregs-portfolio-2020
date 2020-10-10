9d3d0fd7b089fcc3ac7ba9a010f0740e0fef306e
byrneg7 <byrneg7code@gmail.com>
2020-10-10 22:11:26 +0100

    prod creds

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   SENDER_EMAIL: process.env.SENDER_EMAIL,
-  EMAIL_PASS: process.env.PASS,
+  EMAIL_PASS: process.env.EMAIL_PASS,
   RECEIVER_EMAIL: process.env.RECEIVER_EMAIL,
 };
